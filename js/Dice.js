5b9dba9dbc1077e794d56d7ef642413cf0a34845
MatiKHO <matiasproint1@gmail.com>
2024-10-22 12:38:42 +0200

    Task done!

--- js/Dice.js
+++ js/Dice.js
@@ -1,1 +1,10 @@
-class Dice {}
+class Dice {
+    constructor(value) {
+        this.value = Math.floor(Math.random()*6) + 1;
+        this.value = value;
+    }
+
+    roll(value) {
+        return value;
+    }
+}
--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,19 @@
-class Player {}
+class Player {
+    constructor (name, life, attack) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+
+    attackPlayer(player, diceValue) {
+        return player.attack * diceValue;
+    }
+
+    isDead(player) {
+        if (player.life <= 0) {
+            return true;
+        }
+    }
+}
+
+
