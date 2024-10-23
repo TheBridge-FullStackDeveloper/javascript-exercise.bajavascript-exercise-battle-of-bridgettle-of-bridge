1dff12ad4a534e501b490a1f0984e89690f06f84
Will-S <willschrick@gmail.com>
2024-10-23 03:58:13 -0700

    Mon night

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,22 @@
-class Player {}
+class Player {
+    constructor(name,life,attack) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+    
+
+    attackplayer(player, diceValue) {
+         if {
+            player.thislife <=0
+            return 
+         }
+    }
+
+
+    const Player = ()
+    const attackPlayer = new Player("The Rock", 100, attack *2);
+
+
+
+}
