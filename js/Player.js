e26dc4aeab8e0fde4a33bbf873a2bc92ae70855c
JessicaGrotto <jessicalgrotto@gmail.com>
2024-10-23 12:36:46 +0200

    part 1

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,19 @@
-class Player {}
+class Player {
+    constructor(name, life, attack){
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+    attackPlayer(player, diceValue){
+        if(!this.isDead()){
+        const attackResult = this.attack * diceValue
+        return player.life -= attackResult
+        
+        }
+    }
+    isDead(){
+        return (this.life <= 0)
+    }   
+}
+
+    
